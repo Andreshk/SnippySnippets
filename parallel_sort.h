deb1fe19d9f1fd7579b4e11d3dc0a68b911cb6be
Andreshk <andreshko.dre@gmail.com>
2021-02-11 19:37:47 +0200

    major rework of the parallel sorting: - explicitly allocate a scratch buffer for merging & use double buffering - otherwise std::inplace_merge will perform hidden allocations at every call, in every thread! - use std::ranges, some concepts & simplify the interface - unfortunately this incurs an additional copy of the entire scratch buffer in case of odd number ot steps

--- parallel_sort.h
+++ parallel_sort.h
@@ -1,10 +1,15 @@
 #pragma once
-#include <algorithm>    // std::sort, std::inplace_merge
 #include <vector>
 #include <thread>
 #include <atomic>
-#include <iterator>     // std::iterator_traits
-#include <type_traits>  // std::enable_if, std::is_same_v
+#include <iterator>    // std::random_access_iterator
+#include <algorithm>   // std::ranges::sort, std::ranges::merge
+#include <type_traits> // std::enable_if, std::is_same_v
+
+#ifndef vassert
+#include <cassert>
+#define vassert assert
+#endif
 
 // Uncomment for detailed thread interaction info
 //#define PARALLEL_SORT_DEBUG
@@ -15,66 +20,61 @@
 std::mutex coutmtx;
 #endif // PARALLEL_SORT_DEBUG
 
-// Really looking forward to Concepts...
-template <class RanIt>
-inline constexpr bool is_random_iterator = std::is_same_v<typename std::iterator_traits<RanIt>::iterator_category, std::random_access_iterator_tag>;
-
 // Forward declaration
-template<class RanIt, class Pred>
+template<std::ranges::random_access_range Range, class Pred>
 class impl;
 
 // Parallel sort: given n threads, split the array to be sorted in n equal subarrays,
 // sort the subarrays concurrently, and then perform pairwise merging, also in parallel (!)
-template<class RanIt>
-void parallel_sort(RanIt from, RanIt to, size_t nthreads = std::thread::hardware_concurrency()) {
-    // Use std::less<typename std::iterator_traits<RanIt>::value_type>, if C++14 is not available
-    parallel_sort(from, to, nthreads, std::less<>{});
-}
-
 // The comparison predicate should be thread safe in the sense that different threads
 // will call Pred::operator() on different copies of the predicate at the same time.
-template <class RanIt, class Pred>
-std::enable_if_t<is_random_iterator<RanIt>>
-    parallel_sort(RanIt from, RanIt to, size_t nthreads = std::thread::hardware_concurrency(), Pred pr = Pred{})
-{
+template <std::ranges::random_access_range Range, class Pred = std::ranges::less>
+    requires std::sortable<std::ranges::iterator_t<Range>, Pred>
+void parallel_sort(Range& xs, size_t nthreads = std::thread::hardware_concurrency(), Pred pr = Pred{}) {
     // Clamp nthreads to a valid value
-    if (nthreads == 0)
+    if (nthreads == 0) {
         nthreads = 1;
-    const size_t n = size_t(to - from); // Not a narrowing conversion actually
-    if (nthreads > n)
+    }
+    const size_t n = std::ranges::size(xs);
+    if (nthreads > n) {
         nthreads = n;
+    }
     // Create an 'impl' instance, which keeps the internal sorting state
     // (thread pool, counters, subarray limits, etc.) and does all the work.
-    impl<RanIt, Pred> sorter{ from,to,nthreads,pr };
+    impl<Range, Pred> sorter{ xs,nthreads,pr };
 }
 
 // Encapsulates everything needed for parallel sorting and merging:
 // thread array, per-thread subarray info, number of threads active, etc.
-template<class RanIt, class Pred>
+template<std::ranges::random_access_range Range, class Pred>
 class impl {
-    friend void parallel_sort<RanIt, Pred>(RanIt, RanIt, size_t, Pred);
+    friend void parallel_sort<Range, Pred>(Range&, size_t, Pred);
+    using Iter = std::ranges::iterator_t<Range>;
+    using diff_t = typename Iter::difference_type;
 
+    // Scratch memory, to be used during merging
+    std::vector<typename Iter::value_type> scratch;
     std::vector<std::thread> ths;    // thread array
     const size_t nthreads;           // total thread count
-    const size_t nthrpow2;           // smallest power of 2, >= nthreads
+    const size_t numSteps;           // number of steps for the parallel merging (<=> logarithm of smallest power of 2, >= nthreads)
     std::atomic<size_t> ths_created; // number of threads created at a given moment
 #ifdef PARALLEL_SORT_DEBUG
     std::atomic<size_t> ths_joined;  // number of threads, joined after completing their work
 #endif // PARALLEL_SORT_DEBUG
-
-    std::vector<RanIt> froms; // per-thread subarray beginning
-    std::vector<RanIt> tos;   // per-thread subarray end
+    const Iter from;
+    std::vector<diff_t> froms; // per-thread subarray beginning
+    std::vector<diff_t> tos;   // per-thread subarray end
     Pred pr; // comparison predicate
 
-    static size_t smallestPowOf2(const size_t n) {
-        size_t res = 1;
-        while (res < n) res *= 2;
+    static size_t logPowOf2(const size_t n) {
+        size_t res = 0;
+        while ((1ui64 << res) < n) { ++res; }
         return res;
     }
 
-    impl(RanIt from, RanIt to, size_t _nthreads, Pred _pr)
-        : ths{}, nthreads{ _nthreads }, nthrpow2{ smallestPowOf2(_nthreads) }
-        , ths_created{ 0 }, froms{ _nthreads }, tos{ _nthreads } , pr{ _pr }
+    impl(Range& xs, size_t _nthreads, Pred _pr)
+        : scratch(std::ranges::size(xs)), ths{}, nthreads{ _nthreads }, numSteps{ logPowOf2(_nthreads) }
+        , ths_created{ 0 }, from{ std::ranges::begin(xs) }, froms(_nthreads), tos(_nthreads), pr{ _pr }
 #ifdef PARALLEL_SORT_DEBUG
         , ths_joined{ 0 }
 #endif // PARALLEL_SORT_DEBUG
@@ -82,14 +82,15 @@ class impl {
         // Reserve memory to guarantee that no vector reallocations will occur
         // - absolutely necessary for the interactions between threads to work properly (!)
         ths.reserve(nthreads);
-        const size_t subSize = (to - from) / nthreads;
+        const size_t n = std::ranges::size(xs);
+        const size_t subSize = n / nthreads;
         for (size_t idx = 0; idx < nthreads; ++idx) {
             // Calculate the subarrays each thread has to sort
-            froms[idx] = from + subSize*idx;
+            froms[idx] = subSize*idx;
             tos[idx] = froms[idx] + subSize;
         }
         // The final subarray may be a little bit bigger (no more than nthreads more elements than the rest)
-        tos[nthreads - 1] = to;
+        tos[nthreads - 1] = n;
         // Invariant: at _any discrete moment of time_ 'ths_created' is
         // not greater than the # of actually constructed std::threads.
         // This is relied on by the busy-waiting loop in merge_threads().
@@ -102,15 +103,19 @@ class impl {
         ths[0].join();
 #ifdef PARALLEL_SORT_DEBUG
         ++ths_joined;
-        std::cout << "nthreads=" << nthreads << "\nnthrpow2=" << nthrpow2
+        vassert(ths_created == ths_joined);
+        std::cout << "nthreads=" << nthreads << "\nnumSteps=" << numSteps
                   << "\nths_created=" << ths_created << "\nths_joined=" << ths_joined << "\n";
 #endif // PARALLEL_SORT_DEBUG
+        if (numSteps % 2 == 1) {
+            std::ranges::copy(scratch.cbegin(), scratch.cend(), from);
+        }
     }
 
     // Per-thread work: sort each subarray naively, then parallel merge
-    void sort(const size_t idx) {
-        std::sort(froms[idx], tos[idx], pr);
-        merge_threads(idx);
+    void sort(const size_t threadIdx) {
+        std::ranges::sort(from + froms[threadIdx], from + tos[threadIdx], pr);
+        merge_threads(threadIdx);
     }
 
     /* tl;dr: Parallel merge
@@ -123,16 +128,18 @@ class impl {
     * 0    <-   4
     * and afterwards only worker thread #0 will be alive and have the entire array sorted.
     */
-    void merge_threads(const size_t idx) {
-        for (size_t i = 1; i < nthrpow2; i *= 2)
-            if (idx & i) {
+    void merge_threads(const size_t threadIdx) {
+        for (size_t step = 0; step < numSteps; ++step) {
+            const int mask = (1ui64 << step);
+            if (threadIdx & mask) {
                 return; // wait to die
             } else {
                 // Find the victim (thread to merge)...
-                size_t idx_to_kill = idx + i;
+                size_t idx_to_kill = threadIdx + mask;
                 // Nobody left to kill, so wait to die as well
-                if (idx_to_kill >= nthreads)
+                if (idx_to_kill >= nthreads) {
                     return;
+                }
                 // Make sure the victim has actually been created...
                 // (needed when per-thread work takes less time than thread creation)
                 while (idx_to_kill >= ths_created) {}
@@ -141,15 +148,29 @@ class impl {
 #ifdef PARALLEL_SORT_DEBUG
                 ++ths_joined;
                 coutmtx.lock();
-                std::cout << "#" << idx << " <- #" << idx_to_kill << "\n";
+                std::cout << "#" << threadIdx << " <- #" << idx_to_kill << "\n";
                 coutmtx.unlock();
 #endif // PARALLEL_SORT_DEBUG
-                // This thread's subarray and the victim's subarray are actually in consecutive memory.
-                std::inplace_merge(froms[idx], froms[idx_to_kill], tos[idx_to_kill], pr);
+                vassert(tos[threadIdx] == froms[idx_to_kill]);
+                if (step % 2 == 0) {
+                    const Iter start = from + froms[threadIdx];
+                    const Iter mid = from + froms[idx_to_kill];
+                    const Iter end = from + tos[idx_to_kill];
+                    // This thread's subarray and the victim's subarray are actually in consecutive memory.
+                    std::ranges::merge(start, mid, mid, end, scratch.begin() + froms[threadIdx], pr);
+                } else {
+                    const Iter start = scratch.begin() + froms[threadIdx];
+                    const Iter mid = scratch.begin() + froms[idx_to_kill];
+                    const Iter end = scratch.begin() + tos[idx_to_kill];
+                    std::ranges::merge(start, mid, mid, end, from + froms[threadIdx], pr);
+                }
                 // Some false cache sharing will occur here, but no need for
                 // atomic operations - no two threads write to a same location,
                 // and the single read is after all writes to this location.
-                tos[idx] = tos[idx_to_kill];
+                tos[threadIdx] = tos[idx_to_kill];
             }
+        }
     }
 };
+
+#undef vassert
