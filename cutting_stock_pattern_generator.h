6f8c19e7febecc6f0684920b56e46ff33bca56f6
Andreshk <andreshko.dre@gmail.com>
2021-05-27 00:37:48 +0300

    add naive & highly unoptimal recursive pattern generation (via cppcoro::recursive_generator)

--- cutting_stock_pattern_generator.h
+++ cutting_stock_pattern_generator.h
@@ -1,18 +1,49 @@
 #include <cppcoro/generator.hpp>
+#include <cppcoro/recursive_generator.hpp>
 #include <vector>
 #include <span>
 
-// Coroutine, generating all possible patterns for a cutting stock problem.
+// Coroutines, generating all possible patterns for a cutting stock problem.
 // This, of course, is a minor step in the process of solving this problem.
 // Each pattern is identified by the number of occurences of each item.
+
+namespace detail {
+// Helper recursive coroutine that signals each time an entire new pattern is generated.
+// Max recursion depth is equal to the number of items (yikes) - should be taken as a proof of concept only.
+// Note: int is used as a dummy type, should be void ot std::monostate
+cppcoro::recursive_generator<int> helper(const int S, std::span<const int> items, const std::vector<int>::iterator count) {
+	const int x = items.front();
+	if (items.size() == 1) {
+		*count = S / x;
+		co_yield {};
+	} else {
+		for (int i = S / x; i >= 0; --i) {
+			*count = i;
+			co_yield helper(S - i * x, items.subspan(1), std::next(count));
+		}
+	}
+}
+} // namespace detail
+
+// Coroutine, employing the classic, recursive pattern generation technique.
+cppcoro::generator<std::vector<int>> cuttingStockPatternsRec(const int S, std::span<const int> items) {
+	std::vector<int> counts(items.size(), 0); // Number of pieces of each item length in a given pattern
+	// The helper signals each time a pattern is completed -> ignore its result & yield the pattern itself
+	for (auto _ : detail::helper(S, items, counts.begin())) {
+		co_yield counts;
+	}
+}
+
+// An equivalent iterative process that keeps the current recursion "state"
+// in just three integers, resulting in much more efficient pattern generation
+// - no recursive coroutines & coroutine frame allocations, nothing.
 cppcoro::generator<std::vector<int>> cuttingStockPatterns(const int S, std::span<const int> items) {
 	const int n = int(items.size());
 	std::vector<int> counts(n, 0); // Number of pieces of each item length in a given pattern
-	// This simple, inherently recursive process is transformed into an iterative one that keeps the
-	// recursion "state" - current index & whether we are currently back-tracking - at any point in time.
 	int idx = -1; // Index of the current counter to be modified
 	int S1 = S; // Remaining sum (invariant: S1 == S - sumproduct(xs,cnts))
-	bool unwinding = false; // Whether we are currently unwinding the recursion until we reach a non-zero counter, if any
+	bool unwinding = false; // Whether we are currently back-tracking, i.e. unwinding
+	                        // the recursion until we reach a non-zero counter (if any)
 	while (true) {
 		if (unwinding) {
 			if (idx == -1) {
@@ -41,9 +72,10 @@ cppcoro::generator<std::vector<int>> cuttingStockPatterns(const int S, std::span
 
 
 /* Example usage:
+	const int n = 20;
 	const std::vector<int> items{ 9,7,5 };
 	std::cout << "Cutting stock patterns:\n";
-	for (const auto& pat : cuttingStockPatterns(20, items)) {
+	for (const auto& pat : cuttingStockPatterns(n, items)) {
 		for (const int x : pat) { std::cout << x << ' '; }
 		// Compute whatever we need with the current pattern, f.e. the waste
 		const int waste = n - std::inner_product(items.begin(), items.end(), pat.begin(), 0);
